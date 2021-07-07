5a0abc2fd2174d13b55f495d551af473e859114b
Dimitryyyy <67691704+Dimitryyyy@users.noreply.github.com>
2021-07-07 14:15:19 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,25 +1,6 @@
 <?php
-# CLIENT ID
-# https://i.imgur.com/GHI2ts5.png (screenshot)
 $client_id = "629368281513328642";
-
-# CLIENT SECRET
-# https://i.imgur.com/r5dYANR.png (screenshot)
 $secret_id = "61l0dEWRwvs6nnc54S_DqOqzhHsRXz9P";
-
-# SCOPES SEPARATED BY SPACE
-# example: identify email guilds connections  
 $scopes = "identify guilds guilds.join";
-
-# REDIRECT URL
-# example: https://mydomain.com/includes/login.php
-# example: https://mydomain.com/test/includes/login.php
 $redirect_url = "https://skylerbot.ml/";
-
-# IMPORTANT READ THIS:
-# - Set the `$bot_token` to your bot token if you want to use guilds.join scope to add a member to your server
-# - Check login.php for more detailed info on this.
-# - Leave it as it is if you do not want to use 'guilds.join' scope.
-
-# https://i.imgur.com/2tlOI4t.png (screenshot)
-$bot_token = null;
+$client_redirect = "http://skylerbot.ml/auth/redirect";
