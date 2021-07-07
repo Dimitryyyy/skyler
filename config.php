1752cb164045cb931a4c63d5e11a672b1801a2d3
Dimitryyyy <67691704+Dimitryyyy@users.noreply.github.com>
2021-07-07 14:15:57 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,5 +2,4 @@
 $client_id = "629368281513328642";
 $secret_id = "61l0dEWRwvs6nnc54S_DqOqzhHsRXz9P";
 $scopes = "identify guilds guilds.join";
-$redirect_url = "https://skylerbot.ml/";
-$client_redirect = "http://skylerbot.ml/auth/redirect";
+$redirect_url = "http://skylerbot.ml/auth/redirect";
